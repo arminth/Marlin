33f03e9112b203939050bcdf113b0a99938394cb
arminth <armin@a-thinnes.de>
2021-03-21 20:48:43 +0100

    .
    .

--- Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
@@ -35,8 +35,8 @@
 // Use one of these or SDCard-based Emulation will be used
 #if NO_EEPROM_SELECTED
   //#define SRAM_EEPROM_EMULATION                 // Use BackSRAM-based EEPROM emulation
-  //#define FLASH_EEPROM_EMULATION                  // Use Flash-based EEPROM emulation
-  #define I2C_EEPROM
+  #define FLASH_EEPROM_EMULATION                  // Use Flash-based EEPROM emulation
+  //#define I2C_EEPROM
 #endif
 
 #if ENABLED(FLASH_EEPROM_EMULATION)
