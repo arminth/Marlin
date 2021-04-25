433c3a2487d8fa417ee71a52cff8cd35d15de31e
arminth <armin@a-thinnes.de>
2021-04-25 20:54:15 +0200

    initial
    iniz

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1221,11 +1221,11 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 32
+#define X_MIN_POS -1
+#define Y_MIN_POS -34
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE+32
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 
 /**
